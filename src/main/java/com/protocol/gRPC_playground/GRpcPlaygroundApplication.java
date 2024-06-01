475608ebecf5c00910fa112e42b15078b38ce3af
shoaib-ahmed16 <shoaib-ahmed16>
2024-06-01 16:39:19 +0530

    Adding the changes

---
+++ src/main/java/com/protocol/gRPC_playground/GRpcPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.protocol.gRPC_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GRpcPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GRpcPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/protocol/gRPC_playground/GRpcPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protocol.gRPC_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
